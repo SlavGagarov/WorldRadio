9ff1b0e26cdcfca5e5b3954fe05a448660572f99
SlavGagarov <slavgagarov@gmail.com>
2024-04-25 19:00:14 -0500

    with redirect

--- app/src/main/java/com/example/worldradio/MainActivity.kt
+++ app/src/main/java/com/example/worldradio/MainActivity.kt
@@ -3,9 +3,14 @@ package com.example.worldradio
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.OptIn
 import androidx.media3.common.MediaItem
 import androidx.media3.common.Player
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.datasource.DefaultDataSource
+import androidx.media3.datasource.DefaultHttpDataSource
 import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.exoplayer.source.ProgressiveMediaSource
 
 class MainActivity : ComponentActivity() {
 
@@ -26,15 +31,24 @@ class MainActivity : ComponentActivity() {
         releasePlayer()
     }
 
+    @OptIn(UnstableApi::class)
     private fun initializePlayer() {
         player = ExoPlayer.Builder(this)
             .build()
 
+        val httpDataSourceFactory = DefaultHttpDataSource.Factory()
+            .setAllowCrossProtocolRedirects(true)
+
+        val dataSourceFactory = DefaultDataSource.Factory(this, httpDataSourceFactory)
+
         val mediaItem = MediaItem.Builder()
-            .setUri("https://deinradio.stream.laut.fm/deinradio")
+            .setUri("http://radio.garden/api/ara/content/listen/ajJyClv8/channel.mp3")
             .build()
 
-        player.setMediaItem(mediaItem)
+        val mediaSource = ProgressiveMediaSource.Factory(dataSourceFactory)
+            .createMediaSource(mediaItem)
+
+        (player as ExoPlayer).setMediaSource(mediaSource)
         player.playWhenReady = true
         player.prepare()
     }
