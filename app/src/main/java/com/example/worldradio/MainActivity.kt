cda765d774310bb12798bf6bee51e30be7de10dc
SlavGagarov <slavgagarov@gmail.com>
2024-04-25 21:21:18 -0500

    add button to change radio

--- app/src/main/java/com/example/worldradio/MainActivity.kt
+++ app/src/main/java/com/example/worldradio/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.worldradio
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.OptIn
@@ -14,6 +16,9 @@ import androidx.media3.exoplayer.source.ProgressiveMediaSource
 
 class MainActivity : ComponentActivity() {
 
+    private var radioIds = arrayOf("ajJyClv8", "gM0FbQlC", "I9m2o3ys", "HLMePPFo")
+    private var radioPosition = 0
+
     private lateinit var player: Player
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,6 +27,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             // Use Compose UI if needed
         }
+        setContentView(R.layout.activity_main)
 
         initializePlayer()
     }
@@ -42,7 +48,7 @@ class MainActivity : ComponentActivity() {
         val dataSourceFactory = DefaultDataSource.Factory(this, httpDataSourceFactory)
 
         val mediaItem = MediaItem.Builder()
-            .setUri("http://radio.garden/api/ara/content/listen/ajJyClv8/channel.mp3")
+            .setUri("http://radio.garden/api/ara/content/listen/uPX6WnGn/channel.mp3")
             .build()
 
         val mediaSource = ProgressiveMediaSource.Factory(dataSourceFactory)
@@ -55,5 +61,40 @@ class MainActivity : ComponentActivity() {
 
     private fun releasePlayer() {
         player.release()
+    }
+
+    fun onStartButtonClicked(view: View) {
+        nextRadio()
+        Toast.makeText(this, "Playing $radioPosition", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun nextRadio() {
+        if(radioPosition == radioIds.size-1)
+            radioPosition = 0;
+        else
+            radioPosition ++;
+        changeRadio(radioIds[radioPosition])
+    }
+
+    @OptIn(UnstableApi::class)
+    private fun changeRadio(id : String) {
+        player.stop()
+        val audioUrl = "http://radio.garden/api/ara/content/listen/${id}/channel.mp3"
+
+        val httpDataSourceFactory = DefaultHttpDataSource.Factory()
+            .setAllowCrossProtocolRedirects(true)
+
+        val dataSourceFactory = DefaultDataSource.Factory(this, httpDataSourceFactory)
+
+        val mediaItem = MediaItem.Builder()
+            .setUri(audioUrl)
+            .build()
+
+        val mediaSource = ProgressiveMediaSource.Factory(dataSourceFactory)
+            .createMediaSource(mediaItem)
+
+        (player as ExoPlayer).setMediaSource(mediaSource)
+        player.playWhenReady = true
+        player.prepare()
     }
 }
